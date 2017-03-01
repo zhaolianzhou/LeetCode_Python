d1372877706e7e4109c764d7ea63712ccc30d89a
ZhaolianZhou <zhaolianzhou92@gmail.com>
2017-03-01 23:55:17 +1100

    Tree

---
+++ Tree/BinaryTree.py
@@ -0,0 +1,26 @@
+class BinaryTree(object):
+    def insert(self,d):
+        """
+        Add an element to the tree, return the new/modified tree
+        :param d:
+        :return: BinaryTree
+        """
+        pass
+
+    def size(self):
+        """
+        The number of element in the tree
+        :return: int
+        """
+        pass
+    def height(self):
+        """
+        The height of the tree
+        :return: int
+        """
+        pass
+
+    def show(self):
+        """
+        :return:
+        """
